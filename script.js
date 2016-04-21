9c8205cf96af8e83931bf3e557e3aa0f0a502e52
Jacob Zimmerman <jczimm@jczimm.com>
2016-04-20 22:41:42 -0400

    radio: don't do the intro on mobile

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var profileimage = document.querySelector('#profileimage'),
 	body = document.querySelector('body');
-	
-setTimeout(function() {
-	profileimage.className = 'open first';
-	setTimeout(function() {
-		body.className = 'frosted';
 
+if (window.isMobile) {
+	body.className = '';
+	profileimage.className = '';
+} else {
+	setTimeout(function() {
+		profileimage.className = 'open first';
 		setTimeout(function() {
-			body.className = '';
-			profileimage.className = '';
-		}, 350);
-	}, 1000);
-}, 30);
+			body.className = 'frosted';
+
+			setTimeout(function() {
+				body.className = '';
+				profileimage.className = '';
+			}, 350);
+		}, 1000);
+	}, 30);
+}
 
 //
 

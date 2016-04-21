fd9cde2faab754d1deb6d6af20e720751a30c95b
Jacob Zimmerman <jczimm@jczimm.com>
2016-04-20 23:22:50 -0400

    radio: intro's js handling -> css handling

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 var profileimage = document.querySelector('#profileimage'),
 	body = document.querySelector('body');
 
-if (window.isMobile) {
-	body.className = '';
-	profileimage.className = '';
-} else {
+setTimeout(function() {
+	profileimage.className = 'open first';
 	setTimeout(function() {
-		profileimage.className = 'open first';
-		setTimeout(function() {
-			body.className = 'frosted';
+		body.className = 'frosted';
 
-			setTimeout(function() {
-				body.className = '';
-				profileimage.className = '';
-			}, 350);
-		}, 1000);
-	}, 30);
-}
+		setTimeout(function() {
+			body.className = '';
+			profileimage.className = '';
+		}, 350);
+	}, 1000);
+}, 30);
 
 //
 
